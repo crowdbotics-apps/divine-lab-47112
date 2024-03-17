75fbb230eee73c5301d87b7805a7cbd796e909e9
Crowdbotics <team@crowdbotics.com>
2024-03-17 18:41:37 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "divine-lab-47112",
+  url: "https://divine-lab-47112.botics.co",
+  api: "https://divine-lab-47112.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
